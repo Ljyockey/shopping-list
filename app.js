3507430d61354edc9fbc590d582c1338c8360c40
ljyockey <linyockey@gmail.com>
2017-03-21 13:39:26 -0700

    finished remove function and fixed add function

--- app.js
+++ app.js
@@ -2,9 +2,25 @@
 Need to create a variable using the <li>closest() and then find a way to make
 that span adjustable in new variable. Create variable for user input to add*/
 function addItem(input) {
+	if ($(".shopping-list :first").html() === undefined) {
+	var newItem = ('<li><span class="shopping-item">' + input
+		+ '</span>\
+		<div class="shopping-item-controls"> \
+          <button class="shopping-item-toggle">\
+            <span class="button-label">check</span>\
+          </button>\
+          <button class="shopping-item-delete">\
+            <span class="button-label">delete</span>\
+          </button>\
+        </div>\
+      </li>');
+	$(".shopping-list").html(newItem);		
+}
+else {
 	var newItem = $(".shopping-list :first").clone();
 	newItem.find(".shopping-item").text(input);
 	newItem.appendTo(".shopping-list");
+}
 };
 
 /*Create a toggle class for when the check button is clicked*/
@@ -13,11 +29,12 @@ $(".shopping-list").on('click', (".shopping-item-toggle"), function(e) {
 });
 
 /*Remove item when delete is clicked*/
-$("shopping-list").on('click', (".shopping-item-delete"), function(e){
+$(".shopping-list").on('click', (".shopping-item-delete"), function(e) {
 	$(this).closest("li").remove();
 });
 
 $("#js-shopping-list-form button").click(function(e) {
+	//^^Tried using "submit" instead of click and it didn't work.
 	e.preventDefault();
 	var item = $("#shopping-list-entry").val();
 	addItem(item);
